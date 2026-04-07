6d987bf03ff49ed37ec80b3b14628e2301741841
Equipo <fabricio.quezada2501@alumnos.ubibio.cl>
2026-04-07 17:58:55 -0400

    Creacion de atributos y metodos en la clase Rut

--- src/Rut.java
+++ src/Rut.java
@@ -1,6 +1,32 @@
 public class Rut {
 
+        private int numero;
+        private char dv;
+
+        public int getNumero() {
+            return numero;
+        }
+
+        public char getDv() {
+            return dv;
+        }
+
+        private Rut (int numero, char dv){
+            this.numero=numero;
+            this.dv=dv;
 
 
 
-}
+
+        }
+        public void of (String rutCONdv){
+
+
+        }
+
+    }
+
+
+
+
+
