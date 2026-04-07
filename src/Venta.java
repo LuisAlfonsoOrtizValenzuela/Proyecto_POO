7211235a06626ac518ddd9ac3bbf8e77a6377310
Luis <luis.ortiz2501@alumnos.ubiobio.cl>
2026-04-07 18:55:23 -0400

    - Escritura del esqueleto del codigo de "Venta" y "Pasaje", nada implementado a 100%

--- src/Venta.java
+++ src/Venta.java
@@ -1,2 +1,48 @@
+import java.time.LocalDate;
+
 public class Venta {
+
+    private String idDocumento;
+    private TipoDocumento tipo;
+    private LocalDate fecha;
+
+    public Venta (String id, TipoDocumento tipo, LocalDate fec, Cliente cli ){
+        this.idDocumento = id;
+        this.tipo = tipo;
+        this.fecha = fec;
+
+    }
+
+    public String getIdDocumento() {
+        return idDocumento;
+
+    }
+
+    public TipoDocumento getTipo() {
+        return tipo;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public String getCliente(){
+        return null;
+    }
+
+    public void createPasaje(int asiento, Viaje viaje, Pasajero pasajero){
+
+    }
+
+    public Pasaje[] getPasajes(){
+        return null;
+    }
+
+    public int getMonto(){
+        return 0;
+    }
+
+
+
+
 }
