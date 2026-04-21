3d5f2217156a9eeb41b1571663c3d64d0bfaa473
Luis <luis.ortiz2501@alumnos.ubiobio.cl>
2026-04-20 21:05:00 -0400

    -Clase Pasaje actualizada

--- src/Pasaje.java
+++ src/Pasaje.java
@@ -1,16 +1,25 @@
+import java.util.Random;
+
 public class Pasaje {
 
     private long numero;
     private int asiento;
 
+    private Viaje viaje;
+    private Pasajero pasajero;
+    private Venta venta;
 
-    Pasaje(int asiento, Viaje viaje, Pasajero pasajero, Venta venta){
+    public Pasaje(int asiento, Viaje viaje, Pasajero pasajero, Venta venta) {
 
-    }
-
-    public Pasaje(long numero, int asiento) {
-        this.numero = numero;
         this.asiento = asiento;
+
+        this.viaje = viaje;
+        this.pasajero = pasajero;
+        this.venta = venta;
+
+        Random r = new Random();
+        this.numero = Math.abs(r.nextLong());
+
 
     }
 
@@ -22,15 +31,15 @@ public class Pasaje {
         return asiento;
     }
 
-    public Viaje getViaje(){
-        return null;
+    public Viaje getViaje() {
+        return viaje;
     }
 
-    public Pasajero getPasajero(){
-        return null;
+    public Pasajero getPasajero() {
+        return pasajero;
     }
 
-    public Venta getVenta(){
-        return null;
+    public Venta getVenta() {
+        return venta;
     }
 }
