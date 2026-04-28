f7816f629e3bd9355e4bdfb2e91eb2923ac81333
adolfopereira2501-ui <adolfo.pereira2501@alumnos.ubiobio.cl>
2026-04-27 23:42:27 -0400

    Remove Venta class and its methods

--- src/Venta.java
+++ src/Venta.java
@@ -1,19 +1,1 @@
-import java.time.LocalDate;
-import java.util.ArrayList;
 
-public class Venta {
-    private String idDocumento;
-    private TipoDocumento tipoDocumento;
-    private LocalDate fecha;
-    private ArrayList<Cliente> clientes;
-
-    public Venta(String idDocumento, TipoDocumento tipoDocumento, LocalDate fecha, Cliente cliente) {
-        this.idDocumento = idDocumento;
-        this.tipoDocumento = tipoDocumento;
-        this.fecha = fecha;
-    }
-
-    public Cliente getCliente(){
-        return clientes.getLast();
-    }
-}
